92fee5dfdce709cdadb6d4ce7c7c886449e1bb66
Bruno Franco <brunofrancco@gmail.com>
2025-05-22 13:25:47 +0100

    done before publish

--- src/components/map-component.tsx
+++ src/components/map-component.tsx
@@ -1,9 +1,13 @@
 export default function GrupoMaps() {
   return (
     <iframe
-      src="https://www.google.com/maps/@32.647288,-16.8999215,17.13z?hl=pt-PT&entry=ttu&g_ep=EgoyMDI1MDUxNS4wIKXMDSoASAFQAw%3D%3D"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3359.563701274857!2d-16.900139687318486!3d32.64712079050073!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xc6061003ff3a3bd%3A0xc9826d2220404f69!2sGrupo%2092%20AEP%20-%20Funchal!5e0!3m2!1sen!2spt!4v1747915547506!5m2!1sen!2spt"
+      width="600"
+      height="450"
+      className="border-0 w-full"
       allowFullScreen
-      frameBorder="0"
+      loading="lazy"
+      referrerPolicy="no-referrer-when-downgrade"
     ></iframe>
   );
 }
